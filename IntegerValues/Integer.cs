b4e6b3efa63847c90ec4e9e6787f5eda5c078bcf
artemch5a1 <artemch5a1@yandex.ru>
2025-05-02 19:03:05 +0300

    done

--- IntegerValues/Integer.cs
+++ IntegerValues/Integer.cs
@@ -1,7 +1,57 @@
 ﻿namespace IntegerValues
 {
-    public class Integer
+    public static class Integer
     {
+        public static int Round(double number)
+        {
+            // Проверка на переполнение
+            if (number > int.MaxValue || number < int.MinValue)
+            {
+                throw new OverflowException($"Число {number} выходит за пределы диапазона int");
+            }
 
+            // Получаем целую часть
+            int integerPart = (int)number;
+            // Получаем дробную часть
+            double fractionalPart = number - integerPart;
+
+            // Округляем вверх если дробная часть >= 0.5
+            if (fractionalPart >= 0.5)
+            {
+                // Проверка на переполнение при округлении вверх
+                if (number > 0 && integerPart == int.MaxValue)
+                    throw new OverflowException("Округление вызывает переполнение");
+                return integerPart + 1;
+            }
+            // Округляем вниз если дробная часть < -0.5
+            else if (fractionalPart < -0.5)
+            {
+                // Проверка на переполнение при округлении вниз
+                if (number < 0 && integerPart == int.MinValue)
+                    throw new OverflowException("Округление вызывает переполнение");
+                return integerPart - 1;
+            }
+
+            return integerPart;
+        }
+
+        public static bool IsEven(int number)
+        {
+            // Используем побитовую операцию для проверки четности
+            // У четных чисел младший бит всегда 0
+            return (number & 1) == 0;
+        }
+
+        public static int Absolute(int number)
+        {
+            // Особый случай - минимальное значение int
+            if (number == int.MinValue)
+            {
+                throw new OverflowException($"Невозможно взять модуль {number}, так как |int.MinValue| > int.MaxValue");
+            }
+
+            // Если число отрицательное, возвращаем его с противоположным знаком
+            return number < 0 ? -number : number;
+        }
     }
 }
---
+++ TestIntegerValue/Test1.cs
@@ -0,0 +1,70 @@
+﻿using IntegerValues;
+
+namespace TestIntegerValue
+{
+    [TestClass]
+    public sealed class TestInteger
+    {
+        [TestMethod]
+        public void TestRound_ReturnCorrectRound()
+        {
+            //Arrange
+            double a = 15.45;
+            int expected = 15;
+
+            //Act
+            int actual = Integer.Round(a);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestRound_ThorwExeption()
+        {
+            //Arrange
+            double a = 2147483648.0;
+
+            //Act and Assert
+            Assert.ThrowsException<OverflowException>(() => Integer.Round(a));
+        }
+
+
+        [TestMethod]
+        public void TestIsEven_ReturnTrue()
+        {
+            //Arrange
+            int a = 16;
+            bool expected = true;
+
+            //Act
+            bool actual = Integer.IsEven(a);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestAbsolute_ReturnCorrectModel()
+        {
+            //Arrange
+            int a = -16;
+            int expected = 16;
+
+            //Act
+            int actual = Integer.Absolute(a);
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void TestAbsolute_ThorwExeption()
+        {
+            int a = int.MinValue;
+
+            //Act and Assert
+            Assert.ThrowsException<OverflowException>(() => Integer.Absolute(a));
+        }
+    }
+}
